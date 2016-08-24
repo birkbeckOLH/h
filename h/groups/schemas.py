c3fd0255f75c95d169aae757e73e16a8aeb38f72
chdorner <christof@chdorner.com>
2016-08-24 18:46:57 +0100

    Add blacklist for group names
    Since the URL structure is /{pubid}/{slug}, and we have two special URLs
    (/{pubid}/leave and /{pubid}/edit) we have to stop allowing group names
    that end up with slugs that are "edit" or "leave".
    
    This implements a blacklist similar to what we do for usernames, the
    error message says that the chosen group name is not allowed.
    
    Fixes #3775

--- h/groups/schemas.py
+++ h/groups/schemas.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 
+from codecs import open
 import colander
 import deform
+import logging
+
+import slugify
 
 from h import i18n
 from h.accounts.schemas import CSRFSchema
@@ -13,6 +17,32 @@ from h.groups.models import (
 
 
 _ = i18n.TranslationString
+log = logging.getLogger(__name__)
+
+GROUPSLUG_BLACKLIST = None
+
+
+def get_blacklist():
+    global GROUPSLUG_BLACKLIST
+    if GROUPSLUG_BLACKLIST is None:
+        try:
+            with open('h/groups/blacklist', encoding='utf-8') as fp:
+                blacklist = fp.readlines()
+        except (IOError, ValueError):
+            log.exception('unable to load blacklist')
+            blacklist = []
+        GROUPSLUG_BLACKLIST = set(l.strip().lower() for l in blacklist)
+    return GROUPSLUG_BLACKLIST
+
+
+def unblacklisted_group_name_slug(node, value, blacklist=None):
+    """Colander validator that ensures the "slugified" group name is not blacklisted."""
+    if blacklist is None:
+        blacklist = get_blacklist()
+
+    if slugify.slugify(value).lower() in blacklist:
+        raise colander.Invalid(node, _("Sorry, this group name is not allowed. "
+                                       "Please choose another one."))
 
 
 class GroupSchema(CSRFSchema):
@@ -22,9 +52,9 @@ class GroupSchema(CSRFSchema):
     name = colander.SchemaNode(
         colander.String(),
         title=_("Name"),
-        validator=colander.Length(
-            min=GROUP_NAME_MIN_LENGTH,
-            max=GROUP_NAME_MAX_LENGTH),
+        validator=colander.All(
+            colander.Length(min=GROUP_NAME_MIN_LENGTH, max=GROUP_NAME_MAX_LENGTH),
+            unblacklisted_group_name_slug),
         widget=deform.widget.TextInputWidget(
             autofocus=True,
             css_class="group-form__name-input js-group-name-input",
